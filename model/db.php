c365b42622cad64d36ad6d2bb837fcb203b08546
RONALDORS30 <romansalvadorronaldoaldairmail.com>
2025-09-08 01:04:39 -0500

    mi primer comit

---
+++ model/db.php
@@ -0,0 +1,20 @@
+<?php
+
+define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
+define('DB_USER', 'root');//Usuario de tu base de datos
+define('DB_PASS', '');//Contraseña del usuario de la base de datos
+define('DB_NAME', 'restaurantefe');//Nombre de la base de datos
+
+
+define('COLOR', '#58FA82');
+define('COLOR1', '#F5D0A9');
+define('COLOR2', '#58FAAC');
+define('COLOR3', '#F3F781');
+
+
+
+
+
+
+
+?>
