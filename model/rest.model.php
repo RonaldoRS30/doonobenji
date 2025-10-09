d1dc487b90f15a7fd853bcb5d8fbf9f7bd679e36
RONALDORS30 <romansalvadorronaldoaldairmail.com>
2025-10-09 02:46:47 -0500

    Refactorizar la estructura del código para mejorar la legibilidad y el mantenimiento

--- model/rest.model.php
+++ model/rest.model.php
@@ -5,7 +5,7 @@ class Database
     {        
         try
 			{
-				$conexionn = new PDO('mysql:host=localhost;dbname=restaurantefe;charset=utf8', 'root', '');
+				$conexionn = new PDO('mysql:host=localhost;dbname=doono_new;charset=utf8', 'root', '');
 	        	$conexionn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
 	        	return $conexionn;  
 			}
