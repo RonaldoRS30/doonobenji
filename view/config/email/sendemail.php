c6a356313ff6fc3dd0a27f6a1783c6ae8a603024
Jhordan Roly <jhordanroly22@gmail.com>
2025-10-08 21:43:24 -0500

    funcion para enviar factura por correo arreglado solo falta cambiar el nombre de la razon social archivo sendemail.php

--- view/config/email/sendemail.php
+++ view/config/email/sendemail.php
@@ -1,43 +1,47 @@
-<!--Author: Obed Alvarado
-Author URL: http://obedalvarado.pw
-License: Creative Commons Attribution 3.0 Unported
-License URL: http://creativecommons.org/licenses/by/3.0/ !-->
 <?php
-function sendemail($imagen,$mail_setFromName,$mail_addAddress,$txt_message,$mail_subject, $template,$mostrar_mensaje){
-	require 'PHPMailer/PHPMailerAutoload.php';
-	$mail = new PHPMailer;
-	$mail->isSMTP();                            // Establecer el correo electrónico para utilizar SMTP
+function sendemail($imagen, $mail_setFromName, $mail_addAddress, $txt_message, $mail_subject, $template, $mostrar_mensaje) {
+    require 'PHPMailer/PHPMailerAutoload.php';
+    $mail = new PHPMailer;
 
-	$mail->SMTPSecure = 'tls';                  // Habilitar encriptacion, `ssl` es aceptada
-	$mail->Port = 587;                          // Puerto TCP  para conectarse 
+    // Configuración SMTP
+    $mail->isSMTP();
+    $mail->Host       = 'smtp.gmail.com';                  // Servidor SMTP
+    $mail->SMTPAuth   = true;                              // Habilitar autenticación
+    $mail->Username   = 'jhordanroly22@gmail.com';         // Tu correo Gmail
+    $mail->Password   = 'rwjt pjni zkrc xjfb';            // Contraseña de aplicación Gmail
+    $mail->SMTPSecure = 'tls';                             // Seguridad TLS
+    $mail->Port       = 587;                               // Puerto TLS
 
-	$mail->Host = 'smtp.gmail.com';             // Especificar el servidor de correo a utilizar 
-	$mail->SMTPAuth = true;                     // Habilitar la autenticacion con SMTP
-	$mail->Username = "tucorreo@gmail.com";     // Correo electronico saliente ejemplo: tucorreo@gmail.com
-	$mail->Password = "tucontraseña"; 			// Tu contraseña de gmail
-	$mail_setFromEmail = "tucorreo@gmail.com";  // Correo electronico nuevamente
-	
-	$mail->setFrom($mail_setFromEmail, $mail_setFromName);//Introduzca la dirección de la que debe aparecer el correo electrónico. Puede utilizar cualquier dirección que el servidor SMTP acepte como válida. El segundo parámetro opcional para esta función es el nombre que se mostrará como el remitente en lugar de la dirección de correo electrónico en sí.
-	$mail->addReplyTo($mail_setFromEmail, $mail_setFromName);//Introduzca la dirección de la que debe responder. El segundo parámetro opcional para esta función es el nombre que se mostrará para responder
-	$mail->addAddress($mail_addAddress);   // Agregar quien recibe el e-mail enviado
-	$mail->AddEmbeddedImage($imagen, "my-attach", $imagen);
-	$message = file_get_contents($template);
-	$message = str_replace('{{first_name}}', $mail_setFromName, $message);
-	$message = str_replace('{{message}}', $txt_message, $message);
-	$message = str_replace('{{customer_email}}', $mail_setFromEmail, $message);
-	$mail->isHTML(true);  // Establecer el formato de correo electrónico en HTML
-	
-	$mail->Subject = $mail_subject;
-	$mail->msgHTML($message);
-	$envio = $mail->send();
-	if($mostrar_mensaje){
-		if(!$envio) {
-			echo '<p style="color:red">No se pudo enviar el mensaje..';
-			echo 'Error de correo: ' . $mail->ErrorInfo."</p>";
-		} else {
-			echo '<p style="color:green">Tu mensaje ha sido enviado!</p>';
-		}
-	}
-	
+    // Remitente
+    $mail_setFromEmail = 'jhordanroly22@gmail.com';
+    $mail->setFrom($mail_setFromEmail, $mail_setFromName);
+    $mail->addReplyTo($mail_setFromEmail, $mail_setFromName);
+
+    // Destinatario
+    $mail->addAddress($mail_addAddress);
+
+    // Imagen adjunta
+    $mail->AddEmbeddedImage($imagen, "my-attach", $imagen);
+
+    // Cuerpo del mensaje
+    $message = file_get_contents($template);
+    $message = str_replace('{{first_name}}', $mail_setFromName, $message);
+    $message = str_replace('{{message}}', $txt_message, $message);
+    $message = str_replace('{{customer_email}}', $mail_setFromEmail, $message);
+
+    $mail->isHTML(true);
+    $mail->Subject = $mail_subject;
+    $mail->msgHTML($message);
+
+    // Envío
+    $envio = $mail->send();
+    if ($mostrar_mensaje) {
+        if (!$envio) {
+            echo '<p style="color:red">No se pudo enviar el mensaje..';
+            echo 'Error de correo: ' . $mail->ErrorInfo . "</p>";
+        } else {
+            echo '<p style="color:green">Tu mensaje ha sido enviado!</p>';
+        }
+    }
 }
 ?>
